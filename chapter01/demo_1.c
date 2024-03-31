fbd35b2b361d005b1cae2840a68156b0738a0f11
liu13chao <liu13chao@163.com>
2024-03-31 23:20:46 +0800

    feat: chapter_1

---
+++ chapter01/demo_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define FORMAT_STRING "%s"
+#define MESSAGE "Hello, World!\n"
+
+int main(int argc, char* argv[]) {
+    printf(FORMAT_STRING, MESSAGE);
+    return 0;
+}
---
+++ chapter01/demo_1.s
@@ -0,0 +1,35 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 13, 0	sdk_version 13, 3
+	.intel_syntax noprefix
+	.globl	_main                           ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register rbp
+	sub	rsp, 16
+	mov	dword ptr [rbp - 4], 0
+	mov	dword ptr [rbp - 8], edi
+	mov	qword ptr [rbp - 16], rsi
+	lea	rdi, [rip + L_.str]
+	lea	rsi, [rip + L_.str.1]
+	mov	al, 0
+	call	_printf
+	xor	eax, eax
+	add	rsp, 16
+	pop	rbp
+	ret
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"%s"
+
+L_.str.1:                               ## @.str.1
+	.asciz	"Hello, World!\n"
+
+.subsections_via_symbols
